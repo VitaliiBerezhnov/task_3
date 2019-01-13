5ff1295eab0b10810863c68986d07172fa484c99
VitaliiBerezhnov <vitaliiberezhnov@gmail.com>
2019-01-14 00:20:28 +0200

    create loader

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ requirejs(["scripts/showData"], function(showData) {
         xhr.open('GET', 'https://www.reddit.com/r/frontend.json', true);
     
         xhr.onreadystatechange = function() {
+
+            if (xhr.readyState === 3) {
+                var load = document.getElementById('load').style.display = "block";
+            } else {
+                load = document.getElementById('load').style.display = "none";
+            }
             if (xhr.readyState !=4) return;
     
             var data = JSON.parse(xhr.responseText);
--- scripts/createCard.js
+++ scripts/createCard.js
@@ -6,15 +6,19 @@ define(function(){
         var img = document.createElement('IMG');
         var autor = document.createElement('P');
 
-        card.className = 'card';
-        card.appendChild(title);
-        var bookTitle = heading || 'Title';
-        title.innerHTML = bookTitle;
-        title.className = 'title';
-
+        
         card.appendChild(img);
         img.src = src || './img/undefinedIMG.png';
         img.className = 'img';
+
+        card.className = 'card';
+        card.appendChild(title);
+        var bookTitle = heading || 'Title';
+        if(bookTitle.length > 50){
+          bookTitle = bookTitle.slice(0, 50) + '...';
+        }
+        title.innerHTML = bookTitle;
+        title.className = 'title';
 
         card.appendChild(autor);
         autor.innerHTML = name || 'VB';
